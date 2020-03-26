531648100d57c9fb4910e91cf625145510469502
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-26 12:14:55 +0100

    challenge1

---
+++ C# Survival Guide/Assets/Scripts/ChallangeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallangeVariables : MonoBehaviour
+{
+    string myName = "Arber";
+    int myAge = 28;
+    float myspeed = 5.3f;
+    int health = 100;
+    int score = 120;
+    bool hasKey;
+    int ammo = 55;
+
+    void Start()
+    {
+        Debug.Log("My name is " + myName + "and my age is " + myAge);//etc
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
