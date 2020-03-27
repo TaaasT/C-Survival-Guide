7c4a6c1c7141211f4743ba4af281a02dcb6c88ec
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-27 12:48:02 +0100

    cha1

---
+++ C# Survival Guide/Assets/Scripts/Switch statments/Cha1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cha1 : MonoBehaviour
+{
+    public int points;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            points = 50;
+        }
+        else if(Input.GetKeyDown(KeyCode.W))
+        {
+            points = 100;
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            points = 0;
+        }
+
+        switch (points)
+        {
+            case 50:
+                Debug.Log("Points are 50!");
+                break;
+            case 100:
+                Debug.Log("Points are 100!");
+                break;
+            default:
+                Debug.Log("You need 50 or 100 points to recive a message");
+                break;
+        }
+
+
+
+        /*
+        if(points == 50)
+        {
+            Debug.Log("Points are 50!");
+        }
+        else if(points == 100)
+        {
+            Debug.Log("Points are 100!");
+        }
+        else
+        {
+            Debug.Log("You need 50 or 100 points to recive a message");
+        }*/
+    }
+}
