8ed7b138b58bc1337ed31a329acf23d086cb50ae
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-27 10:53:26 +0100

    if

---
+++ C# Survival Guide/Assets/Scripts/IfStatments/Challenge1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge1 : MonoBehaviour
+{
+
+    [SerializeField] int score;
+    [SerializeField] int points = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            score += points;
+        }
+
+        if(score >= 50)
+        {
+            Debug.Log("You are awsome");
+        }
+    }
+}
