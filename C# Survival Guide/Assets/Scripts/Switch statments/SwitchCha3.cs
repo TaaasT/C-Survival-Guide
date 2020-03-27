f641c6af2a05951302dceb88a9bc1d597c43b5d1
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-27 13:31:24 +0100

    cha4

---
+++ C# Survival Guide/Assets/Scripts/Switch statments/SwitchCha3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCha3 : MonoBehaviour
+{
+    public int weaponID;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(weaponID)
+        {
+            case 1:
+                Debug.Log("Gun");
+                break;
+            case 2:
+                Debug.Log("Knife");
+                break;
+            case 3:
+                Debug.Log("Machine Gun");
+                break;
+            default:
+                Debug.Log("Invalid Weapon");
+                break;
+        }
+    }
+}
