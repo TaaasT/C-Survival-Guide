445a0a30db5ea05ef00c10ab70989061388bd74f
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-27 13:03:05 +0100

    cha2

---
+++ C# Survival Guide/Assets/Scripts/Switch statments/Cha2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cha2 : MonoBehaviour
+{
+    public GameObject cube;
+
+    private int nextColor;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            nextColor = 0;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            nextColor = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            nextColor = 2;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            nextColor = 3;
+        }
+
+        switch(nextColor)
+        {
+            case 0:
+                cube.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case 1:
+                cube.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case 2:
+                cube.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case 3:
+                cube.GetComponent<Renderer>().material.color = Color.black;
+                break;
+            default:
+                Debug.Log("Invalid selection");
+                break;
+        }
+    }
+
+}
