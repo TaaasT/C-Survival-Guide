3f252b66a831e03ca91c2f0e82009adf0a8c4442
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-28 11:14:03 +0100

    0

--- C# Survival Guide/Assets/Scripts/loop/loopcha1.cs
+++ C# Survival Guide/Assets/Scripts/loop/loopcha1.cs
@@ -7,12 +7,37 @@ public class loopcha1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                if(i < 10)
+                {
+                    Debug.Log(i);
+                }
+                
+
+                if(i > 9 && i < 21)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Debug.Log(i);
+                    }
+                }
+                else if(i > 20)
+                {
+                    if(i % 2 == 1)
+                    {
+                        Debug.Log(i);
+                    }
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
