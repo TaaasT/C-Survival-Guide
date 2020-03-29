45d99e0c7b70e6588ff4d8888856ef83db98c0ac
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-29 11:15:42 +0200

    loopcha2

---
+++ C# Survival Guide/Assets/Scripts/loop/LoopCha2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopCha2 : MonoBehaviour
+{
+    public int speed;
+    public int maxSpeed;
+
+    void Start()
+    {
+        maxSpeed = Random.Range(60, 120);
+        StartCoroutine(SpeedRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpeedRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            speed += 5;
+
+            if(speed > maxSpeed)
+            {
+                break;
+            }
+        }
+        
+    }
+}
