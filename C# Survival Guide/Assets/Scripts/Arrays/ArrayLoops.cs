83423414f7f347f76f8dec3088b4358bf7be1339
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 13:00:50 +0200

    1

--- C# Survival Guide/Assets/Scripts/Arrays/ArrayLoops.cs
+++ C# Survival Guide/Assets/Scripts/Arrays/ArrayLoops.cs
@@ -2,19 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Item
+{
+    public int itemID;
+    public string name;
+    public string description;
+}
+
 public class ArrayLoops : MonoBehaviour
 {
-    public string[] itemName;
+    //public string[] itemName;
+
+    public Item[] myItems;
 
     void Start()
     {
-        
+        /*foreach(var item in myItems)
+        {
+            Debug.Log(item.name);
+        }
+
+        foreach(var item in myItems)
+        {
+            if(item.itemID == 7)
+            {
+                Debug.Log("you have it!");
+            }
+            else
+            {
+                Debug.Log("You dont have this item");
+            }
+        }*/
+
+        for(int i = 0; i < myItems.Length; i++)
+        {
+            if(myItems[i].itemID == 7)
+            {
+                Debug.Log("You have item!");
+            }
+            else
+            {
+                Debug.Log("You do not have item");
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+       /* if(Input.GetKeyDown(KeyCode.Space))
         {
             for(int i = 0; i < itemName.Length; i++)
             {
@@ -28,6 +65,16 @@ public class ArrayLoops : MonoBehaviour
                     Debug.Log(item);
                 }
             }
-        }
+        }*/
+
+        /*if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int randomID = Random.Range(0, myItems.Length);
+            
+            for (int i = 0; i < myItems.Length; i++)
+            {
+                Debug.Log(myItems[randomID].name);
+            }
+        }*/
     }
 }
