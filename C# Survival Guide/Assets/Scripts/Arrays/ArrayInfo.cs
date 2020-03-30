1e0a46a661b3d9a7945e104052eb0242bb633b66
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 11:50:51 +0200

    info

--- C# Survival Guide/Assets/Scripts/Arrays/ArrayInfo.cs
+++ C# Survival Guide/Assets/Scripts/Arrays/ArrayInfo.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class ArrayInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // three ways of doing an array
+
+    // declare the size (how many elements) through the inspector. most common
+
+    //public string[] names;
+
+    // second way rarly used
+    //public string[] items = new string[5];
+
+    // third way is to specify in code what those are. declaring size and populating the information now.
+    //public int[] ages = new int[] { 5, 4, 3, 55, 66 };
+
     void Start()
     {
-        
+      //  Debug.Log(names[0]);
+        //Debug.Log(ages[3]);
     }
 
     // Update is called once per frame
