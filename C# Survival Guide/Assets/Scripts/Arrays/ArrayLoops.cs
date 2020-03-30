56717ed11b2700af2ebbe2525551a9c225485b5f
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 12:49:02 +0200

    arrayloops

---
+++ C# Survival Guide/Assets/Scripts/Arrays/ArrayLoops.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayLoops : MonoBehaviour
+{
+    public string[] itemName;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i = 0; i < itemName.Length; i++)
+            {
+                Debug.Log(itemName[i]);
+            }
+
+            foreach(var item in itemName)
+            {
+                if (item == "Sword")
+                {
+                    Debug.Log(item);
+                }
+            }
+        }
+    }
+}
