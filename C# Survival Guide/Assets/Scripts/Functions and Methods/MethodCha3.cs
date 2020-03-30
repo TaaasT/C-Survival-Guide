fdc9f8a5d8480f82566707daf8f43f1e131870f5
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 14:16:07 +0200

    methods cha3

---
+++ C# Survival Guide/Assets/Scripts/Functions and Methods/MethodCha3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodCha3 : MonoBehaviour
+{
+    public Vector3[] positions;
+    private int randomIndex;
+
+
+    void Start()
+    {
+        randomIndex = GetRandom();
+
+        Debug.Log("random index " + randomIndex);
+
+        transform.position = GetPosition(randomIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int GetRandom()
+    {
+        return Random.Range(0, positions.Length);
+    }
+
+    Vector3 GetPosition(int index)
+    {
+        return positions[index];
+    }
+}
