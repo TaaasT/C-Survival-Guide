4e56c0237bac4f95371f6558cc870f362e59b8b9
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 11:59:57 +0200

    Cha1

--- C# Survival Guide/Assets/Scripts/Arrays/ArraysCha1.cs
+++ C# Survival Guide/Assets/Scripts/Arrays/ArraysCha1.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ArraysCha1 : MonoBehaviour
 {
 
+    public string[] names;
+    public int[] age;
+    public string[] carmodel;
+
     void Start()
     {
 
@@ -13,6 +17,12 @@ public class ArraysCha1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(names[4]);
+            Debug.Log(age[4]);
+            Debug.Log(carmodel[4]);
+        }
+
     }
 }
