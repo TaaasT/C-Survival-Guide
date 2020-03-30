d7147d5ea301670e7c7fb30f4c78146d7b6dd160
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 14:02:03 +0200

    cha2

---
+++ C# Survival Guide/Assets/Scripts/Functions and Methods/MethodCha2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodCha2 : MonoBehaviour
+{
+    public int health = 100;
+    public bool isDead;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Damage(Random.Range(1, 10));
+    }
+
+    void Damage(int damageAmount)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isDead == false)
+        {
+            health -= damageAmount;
+
+            if (health < 1)
+            {
+                isDead = true;
+                health = 0;
+                Debug.Log("Player Has Died");
+            }
+        }
+    }
+}
