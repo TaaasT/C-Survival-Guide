fd81a0d2dbebd1607b1db7ffbabe196939e17a1e
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 11:45:49 +0200

    arrays

---
+++ C# Survival Guide/Assets/Scripts/Arrays/ArraysCha1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysCha1 : MonoBehaviour
+{
+    // three ways of doing an array
+
+    // declare the size (how many elements) through the inspector. most common
+
+    //public string[] names;
+
+    // second way rarly used
+    //public string[] items = new string[5];
+
+    // third way is to specify in code what those are. declaring size and populating the information now.
+    //public int[] ages = new int[] { 5, 4, 3, 55, 66 };
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
