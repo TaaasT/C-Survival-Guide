4c2c414bc6a378f4025e80e104286a1b21abebbc
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-30 13:41:14 +0200

    methodcha1

---
+++ C# Survival Guide/Assets/Scripts/Functions and Methods/Method.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Method : MonoBehaviour
+{
+    public int health;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage(5);
+        }
+    }
+
+    void Damage(int damageAmound)
+    {
+        health -= damageAmound;
+
+        if(health < 1)
+        {
+            health = 0;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Functions and Methods/MethodCha1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodCha1 : MonoBehaviour
+{
+    public GameObject cube;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeColor();
+    }
+
+    void ChangeColor()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        cube.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+}
