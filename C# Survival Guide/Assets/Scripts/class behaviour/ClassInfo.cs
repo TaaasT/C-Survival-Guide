343d321b6fbe569df00e740188862c4ff9191129
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-31 09:43:58 +0200

    class behaviour info

---
+++ C# Survival Guide/Assets/Scripts/class behaviour/ClassInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassInfo : MonoBehaviour
+{
+    ClassInfo2 sword;
+    ClassInfo2 hammer;
+    ClassInfo2 bread;
+
+    void Start()
+    {
+        // two diffrent ways to make an item.
+        sword = new ClassInfo2();
+        sword.name = "Sword";
+        sword.id = 1;
+        sword.description = "This is a sword!";
+
+        hammer = new ClassInfo2("Hammer", 2, "This is a hammer.");
+        
+        //third way to do same thing.
+        bread = CreateItem("bread", 3, "I can eat this.");
+    }
+    
+  private ClassInfo2 CreateItem(string name, int id, string description)
+    {
+        ClassInfo2 item = new ClassInfo2(name, id, description);
+
+        return item;
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/class behaviour/ClassInfo2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//måste göra såhär annars kan vi inte se det i unity!
+[System.Serializable]
+public class ClassInfo2 
+{
+    public string name;
+    public int id;
+    public string description;
+
+    //i can leave this constructor empty if i want 
+    public ClassInfo2()
+    {
+
+    }
+    //and make another constructor that holds params.
+    public ClassInfo2(string name, int id, string description)
+    {
+        this.name = name;
+        this.id = id;
+        this.description = description;
+    }
+}
