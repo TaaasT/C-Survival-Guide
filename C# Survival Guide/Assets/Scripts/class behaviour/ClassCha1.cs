1bb9a05e5541b5940ec880a906d9c484ed3078d7
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-31 10:03:50 +0200

    Class cha1

---
+++ C# Survival Guide/Assets/Scripts/class behaviour/ClassCha1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassCha1 : MonoBehaviour
+{
+    public Customer john;
+    public Customer jannet;
+    public Customer jessica;
+
+    void Start()
+    {
+        john = new Customer("John", "Svensson", 24, "Male", "Byggare");
+        jannet = new Customer("Jannet", "", 55, "Female", "Instructor");
+        jessica = new Customer("Jessica", "Lang", 23, "Female", "Scientist");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
