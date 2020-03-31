930fbfff20f27413226533f974ba8e323312db98
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-31 11:11:05 +0200

    virtual methods & overriding

---
+++ C# Survival Guide/Assets/Scripts/class behaviour/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    public string petName;
+
+    protected virtual void Speak()
+    {
+        Debug.Log("Speak!");
+    }
+
+    private void Start()
+    {
+        Speak();
+    }
+
+}
