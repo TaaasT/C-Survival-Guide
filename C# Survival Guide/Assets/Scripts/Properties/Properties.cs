789cf93084f090a35d8976a8a85f3053e2be529f
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-01 11:21:32 +0200

    Properties

---
+++ C# Survival Guide/Assets/Scripts/Properties/Properties.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+    private bool isGameOver;
+
+    public bool IsGameOver
+    {
+        get
+        {
+            return isGameOver;
+        }
+        set
+        {
+            if (value == true)
+            {
+                Debug.Log("Ohnoo! game over");
+            }
+            isGameOver = value;
+        }
+    }
+
+    void Start()
+    {
+        isGameOver = false;  
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGameOver = true;
+        }
+    }
+}
