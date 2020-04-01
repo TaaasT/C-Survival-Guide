24a039c35c5bb0910a52e658e806bdb285075538
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-01 11:28:41 +0200

    AutoProp.

---
+++ C# Survival Guide/Assets/Scripts/Properties/AutoProperties.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoProperties : MonoBehaviour
+{
+   public bool IsGameOver { get; private set; }
+
+    void Start()
+    {
+        IsGameOver = false;
+
+        if(IsGameOver)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
