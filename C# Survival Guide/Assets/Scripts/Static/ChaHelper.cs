b377fa59cdcafa7c2a98eb6d2eca5e4f8ccda45d
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-01 11:05:48 +0200

    static cha1

---
+++ C# Survival Guide/Assets/Scripts/Static/ChaHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ChaHelper
+{
+
+    public static void ChangeColor(GameObject obj, Color color, bool randomColor = false)
+    {
+        if(randomColor == true)
+        {
+            color = new Color(Random.value, Random.value, Random.value);
+        }
+        obj.GetComponent<MeshRenderer>().material.color = color;
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Static/StaticCha1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCha1 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChaHelper.ChangeColor(this.gameObject, Color.red, true);
+        }
+    }
+}
