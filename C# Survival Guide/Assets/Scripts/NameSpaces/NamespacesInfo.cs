ba1f3b43c4b4413a3c17d4c0ed9c526dc05a32aa
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-01 11:53:09 +0200

    Namespaces

---
+++ C# Survival Guide/Assets/Scripts/NameSpaces/NamespacesInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+//Namespaces är det som e ovan 
+// anledningen till att man lägger till dom efter behov är för att inte allt ska finnas på en gång o finnas 1000 saker av samma ord.
+
+    
+public class NamespacesInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
