688cde315be156a2b5fbfd86c8d3f8d1d637e434
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-01 10:47:26 +0200

    Utility

---
+++ C# Survival Guide/Assets/Scripts/Static/StaticPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticPlayer : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            UtilityClasses.CreateObject();
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            UtilityClasses.SetPositionToZero(this.gameObject);
+        }
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Static/UtilityClasses.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// by making a class static it cannot inherit anything.
+// and everything in this class has to be static in order for it to work.
+public static class UtilityClasses
+{
+    public static void CreateObject()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+    public static void SetPositionToZero(GameObject obj)
+    {
+        obj.transform.position = Vector3.zero;
+    }
+
+}
