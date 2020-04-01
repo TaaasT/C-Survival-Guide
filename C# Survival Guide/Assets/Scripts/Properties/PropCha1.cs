dcc64e8a40059daeb7c65cde4e1c933519d3c6db
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-01 11:45:38 +0200

    properties cha

---
+++ C# Survival Guide/Assets/Scripts/Properties/PropCha1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropCha1 : MonoBehaviour
+{
+
+    // autoproperty
+    public float speed { get; private set; }
+
+    //property
+    /*public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+        private set
+        {
+            speed = value;
+        }
+
+    }*/
+
+    void Start()
+    {
+        /*Speed = 10f;
+        Debug.Log(Speed);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
