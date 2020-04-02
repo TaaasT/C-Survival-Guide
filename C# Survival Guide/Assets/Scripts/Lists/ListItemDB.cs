9da372b72a4713761ecdbc45e3384c132419d70e
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-02 11:33:29 +0200

    ItemDB

--- C# Survival Guide/Assets/Scripts/Lists/ListItemDB.cs
+++ C# Survival Guide/Assets/Scripts/Lists/ListItemDB.cs
@@ -5,4 +5,24 @@ using UnityEngine;
 public class ListItemDB : MonoBehaviour
 {
     public List<ListItem> itemDataBase = new List<ListItem>();
+
+  
+    public void AddItem(int itemID)
+    {
+        foreach (var item in itemDataBase)
+        {
+            if (item.id == itemID)
+            {
+                Debug.Log("WE have a match!");
+                return;
+            }
+        }
+        Debug.Log("Item does not exist!");
+    }
+
+    public void RemoveItem(int ItemID)
+    {
+
+    }
+
 }
---
+++ C# Survival Guide/Assets/Scripts/Lists/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public ListItem[] inventory;
+
+    private ListItemDB itemDataBase;
+
+    private void Start()
+    {
+        itemDataBase = GameObject.Find("ItemDB").GetComponent<ListItemDB>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            itemDataBase.AddItem(0);
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            itemDataBase.RemoveItem(0);
+        }
+    }
+}
