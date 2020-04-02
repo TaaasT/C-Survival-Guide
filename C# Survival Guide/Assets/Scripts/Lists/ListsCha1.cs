cf9f48b5c5db5dddf72fb44529fd587f5ec0dc28
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-02 10:42:46 +0200

    list cha1

---
+++ C# Survival Guide/Assets/Scripts/Lists/ListsCha1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListsCha1 : MonoBehaviour
+{
+    public List<string> names = new List<string>();
+
+    void Start()
+    {
+        foreach( var name in names)
+        {
+            Debug.Log(name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var nameToRemove = names[Random.Range(0, names.Count)];
+
+            names.Remove(nameToRemove);
+            
+            foreach (var name in names)
+            {
+                Debug.Log(name);
+            }
+
+            Debug.Log("We removed " + nameToRemove);
+
+        }
+    }
+}
