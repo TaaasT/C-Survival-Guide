845d048154ed43556b0815a96d71c48909ea2140
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-02 11:23:03 +0200

    DB

---
+++ C# Survival Guide/Assets/Scripts/Lists/ListItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ListItem
+{
+    public string itemName;
+    public int id;
+    public Sprite icon;
+}
---
+++ C# Survival Guide/Assets/Scripts/Lists/ListItemDB.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListItemDB : MonoBehaviour
+{
+    public List<ListItem> itemDataBase = new List<ListItem>();
+}
