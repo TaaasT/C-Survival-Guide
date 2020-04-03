9783e899e18537b7d4fee1f7ff76a130ab62f55c
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-03 10:29:42 +0200

    en

---
+++ C# Survival Guide/Assets/Scripts/Enums/EnumItemDB.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumItemDB : MonoBehaviour
+{
+    public List<EnumsClass> itemDB = new List<EnumsClass>();
+
+   
+    void Start()
+    {
+        itemDB[0].Action();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Enums/EnumsClass.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnumsClass
+{
+    public string name;
+    public int ID;
+    public Sprite icon;
+
+    public enum ItemType
+    {
+        None,
+        Weapon,
+        Consumable
+    }
+
+    public ItemType itemType;
+
+    public void Action()
+    {
+        switch(itemType)
+        {
+            case ItemType.Weapon:
+                Debug.Log("This is a weapon");
+                break;
+
+            case ItemType.Consumable:
+                Debug.Log("This is a consumable");
+                break;
+
+            case ItemType.None:
+                Debug.Log("There is no type");
+                break;
+
+        }
+    }
+
+}
