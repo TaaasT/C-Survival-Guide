bb58c64276581fd524da53de651f6a5178f39ec5
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-03 10:18:35 +0200

    enumsEnemyAI

---
+++ C# Survival Guide/Assets/Scripts/Enums/EnumsEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumsEnemy : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        Patroling,
+        Attacking,
+        Chasing,
+        Death
+    }
+
+    public EnemyState currentState;
+
+    void Start()
+    {
+        currentState = EnemyState.Patroling;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(currentState)
+        {
+            case EnemyState.Patroling:
+                Debug.Log("Patroling");
+                if(Time.time > 5)
+                {
+                    currentState = EnemyState.Chasing;
+                }
+                break;
+            case EnemyState.Attacking:
+                Debug.Log("Attacking");
+                break;
+            case EnemyState.Chasing:
+                Debug.Log("Chasing");
+                break;
+            case EnemyState.Death:
+                Debug.Log("Death");
+                break;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            currentState = EnemyState.Attacking;
+        }
+
+    }
+}
