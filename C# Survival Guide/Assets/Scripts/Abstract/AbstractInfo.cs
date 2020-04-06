eb3d37d2691f8e6e255f838a6e3f695ddc741bce
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-06 10:35:29 +0200

    Abstract

---
+++ C# Survival Guide/Assets/Scripts/Abstract/AbstractInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractInfo : MonoBehaviour
+{
+    public int speed;
+    public int health;
+    public int gems;
+
+    //forces inheritance on other scripts
+    public abstract void Attack();
+    
+    public virtual void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
+
+
+public class MossGiant : AbstractInfo
+{
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void Die()
+    {
+        // costom particles
+        base.Die();
+    }
+
+}
