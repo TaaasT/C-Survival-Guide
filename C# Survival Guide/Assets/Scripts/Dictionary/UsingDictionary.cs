477502a83d54cd2aac9e031f43e2d2584da09892
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-06 10:27:50 +0200

    Dictionary.1

---
+++ C# Survival Guide/Assets/Scripts/Dictionary/UsingDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingDictionary : MonoBehaviour
+{
+    /*public Dictionary<string, int> people = new Dictionary<string, int>();*/
+
+    public Dictionary<int, string> books = new Dictionary<int, string>();
+
+    void Start()
+    {
+        /*people.Add("Jon", 26);
+        people.Add("James", 55);
+        people.Add("Rachel", 31);
+        people.Add("Yin", 34);
+
+        int rachelsAge = people["Rachel"];
+
+        Debug.Log("Rachels Age: " + rachelsAge);*/
+
+        books.Add(0, "Ultimate guide");
+        books.Add(1, "The Unity C# Surv.");
+
+        foreach(KeyValuePair<int, string> book in books)
+        {
+            Debug.Log("Book: " + book.Value);
+        }
+
+    }
+
+}
