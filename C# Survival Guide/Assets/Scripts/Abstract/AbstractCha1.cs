e4f414a25ca4337e16d174e33c5f45a9fce3755b
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-06 10:41:35 +0200

    Abstract Cha

---
+++ C# Survival Guide/Assets/Scripts/Abstract/AbstractCha1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractCha1 : MonoBehaviour
+{
+    public static string company = "GameDevHQ";
+    public string employeeName;
+
+    public abstract void CalculateMonthlySalary();
+}
+
+
+public class PartTime : AbstractCha1
+{
+    public int hoursWorked;
+    public int hourlyRate;
+
+
+    public override void CalculateMonthlySalary()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+
+public class FullTime : AbstractCha1
+{
+    public int Salary;
+
+    public override void CalculateMonthlySalary()
+    {
+        throw new System.NotImplementedException();
+    }
+}
