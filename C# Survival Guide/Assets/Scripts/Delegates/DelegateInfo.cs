ab047e3216a36ae0ee0f5279b1cacec88b3e0e05
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-07 15:20:49 +0200

    Delegates

---
+++ C# Survival Guide/Assets/Scripts/Delegates/DelegateInfo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateInfo : MonoBehaviour
+{
+    public delegate void ChangeColor(Color newColor);
+    public ChangeColor onColorChange;
+
+    public delegate void OnComplete();
+    public OnComplete onComplete;
+
+    private void Start()
+    {
+        onColorChange = UpdateColor;
+        onColorChange(Color.green);
+        
+        // man kan ha flera methods i samma delegate.
+        onComplete += Task;
+        onComplete += Task2;
+        onComplete += Task3;
+
+        //kan åxo ta bort dom med -=
+        onComplete -= Task3;
+
+        if (onComplete != null)
+        {
+            onComplete();
+        }
+    }
+
+    public void UpdateColor(Color newColor)
+    {
+        Debug.Log("Changing color to: " + newColor.ToString());
+    }
+
+    public void Task()
+    {
+        Debug.Log("Task 1 Finished");
+    }
+
+    public void Task2()
+    {
+        Debug.Log("Task 2 Finished");
+    }
+
+    public void Task3()
+    {
+        Debug.Log("Task 3 Finished");
+    }
+}
