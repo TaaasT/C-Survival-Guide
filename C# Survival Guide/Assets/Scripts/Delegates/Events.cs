5c795cf6b70e1e67e7960179d012d7bcdd04f65c
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-07 15:31:01 +0200

    Events

---
+++ C# Survival Guide/Assets/Scripts/Delegates/Events.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+
+
+  public void ButtonClick()
+    {
+        if (onClick != null)
+        {
+            onClick();
+        }
+    }
+
+
+}
