2789ead8c4374aae01c6b0a14821c0a371cd9804
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-08 09:24:44 +0200

    delegate cha

---
+++ C# Survival Guide/Assets/Scripts/Delegates/CubeCha.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCha : MonoBehaviour
+{
+
+    private void Start()
+    {
+        DelCha.onTeleport += Spawn;
+    }
+
+    public void Spawn(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+
+    private void OnDisable()
+    {
+        DelCha.onTeleport -= Spawn;
+    }
+
+}
---
+++ C# Survival Guide/Assets/Scripts/Delegates/DelCha.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelCha : MonoBehaviour
+{
+    public delegate void Teleport(Vector3 pos);
+    public static event Teleport onTeleport;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onTeleport != null)
+            {
+                Vector3 pos = new Vector3(5, 2, 0);
+                onTeleport(pos);
+            }
+        }
+
+    }
+}
