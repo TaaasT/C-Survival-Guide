6efb032cffaba91ee50565b61ffd0fcb93750cb3
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-09 10:07:00 +0200

    LamdaExpression

---
+++ C# Survival Guide/Assets/Scripts/Delegates/LambdaExpression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaExpression : MonoBehaviour
+{
+    public Func<string, int> CharacterLength;
+
+    void Start()
+    {
+        //detta e lambda expression. det e samma sak längst nere.
+        CharacterLength = (name) => name.Length;
+
+        int count = CharacterLength("Jonathan");
+
+        Debug.Log("Count " + count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //som detta!
+    /*int GetCharacters(string name)
+    {
+        return name.Length;
+    }*/
+
+}
