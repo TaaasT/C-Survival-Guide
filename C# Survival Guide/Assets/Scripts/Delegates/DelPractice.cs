428b4335e7dbe72777b2a200be930907a484919b
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-09 10:22:50 +0200

    Lambda practice

---
+++ C# Survival Guide/Assets/Scripts/Delegates/DelPractice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DelPractice : MonoBehaviour
+{
+    public Action<int, int> Sum;
+
+    private void Start()
+    {
+        Sum = (a, b) =>
+        {
+            var total = a + b;
+            if (total < 100)
+            {
+                Debug.Log("Total is less than 100");
+            }
+
+            Debug.Log("Total " + total);
+        };
+
+        Sum(5, 2);
+    }
+
+}
