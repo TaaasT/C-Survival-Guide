7de93a36ec08c94ef667dba517e4044b9c43230e
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-10 16:54:10 +0200

    a

--- C# Survival Guide/Assets/Scripts/LINQ/LINQQuerys.cs
+++ C# Survival Guide/Assets/Scripts/LINQ/LINQQuerys.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class LINQQuerys : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
 }
