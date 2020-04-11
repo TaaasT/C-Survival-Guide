646d66ea12556b58924d31903359420b2fdded75
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-11 10:46:30 +0200

    Any

--- C# Survival Guide/Assets/Scripts/LINQ/LINQQuerys.cs
+++ C# Survival Guide/Assets/Scripts/LINQ/LINQQuerys.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LINQQuerys : MonoBehaviour
 {
-    
+    public string[] names = { "jon", "alex", "julie", "jessie", "david", "mark", "matt" };
 
     void Start()
     {
-        
+        var nameFound = names.Any(name => name == "jon");
+
+        Debug.Log("Name found: " + nameFound);
     }
 
     void Update()
