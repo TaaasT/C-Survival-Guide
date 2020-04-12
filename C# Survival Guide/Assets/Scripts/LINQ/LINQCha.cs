a23e8bf58356921c01b6165a5f9fb5b6a803cfb4
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-12 11:12:48 +0200

    wharecha

---
+++ C# Survival Guide/Assets/Scripts/LINQ/LINQCha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LINQCha : MonoBehaviour
+{
+    public int[] quizGrades = { 44, 55, 32, 77, 95, 99, 88 };
+
+
+    void Start()
+    {
+        var passingGrades = quizGrades.Where(qg => qg > 69); 
+        
+        foreach(var grade in passingGrades)
+        {
+            Debug.Log("Grade: " + grade);
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
