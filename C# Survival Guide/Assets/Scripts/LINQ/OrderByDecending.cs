71807ea9f441bd39e930b42a81c2117ad9906dcf
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-12 11:15:47 +0200

    orderbydecending

---
+++ C# Survival Guide/Assets/Scripts/LINQ/OrderByDecending.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class OrderByDecending : MonoBehaviour
+{
+    public int[] quizGrades = { 44, 55, 32, 77, 95, 99, 88 };
+
+
+    void Start()
+    {                                                      // fixar dom i ordning.// vänder ordningen
+        var passingGrades = quizGrades.Where(qg => qg > 69).OrderByDescending(g => g).Reverse();
+
+        foreach (var grade in passingGrades)
+        {
+            Debug.Log("Grade: " + grade);
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+}
