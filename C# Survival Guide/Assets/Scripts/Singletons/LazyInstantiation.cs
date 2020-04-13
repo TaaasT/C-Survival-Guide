be216ce9eab5cc501827f69b3230425785408029
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-13 11:15:33 +0200

    LazyInstatiation

---
+++ C# Survival Guide/Assets/Scripts/Singletons/LazyInstantiation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazyInstantiation : MonoBehaviour
+{
+    private static LazyInstantiation _instance;
+    public static LazyInstantiation Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("UI Manager");
+                go.AddComponent<SingUIManager>();
+            }
+            
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
