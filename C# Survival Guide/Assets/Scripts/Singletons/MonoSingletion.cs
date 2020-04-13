9e6fec12a657b4089ebbdc4583b37ff4a26470e9
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-13 11:31:42 +0200

    Monosingleton

---
+++ C# Survival Guide/Assets/Scripts/Singletons/MonoSingletion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingletion<T> : MonoBehaviour where T : MonoSingletion<T>
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.Log(typeof(T).ToString() + " is Null");
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this as T;
+
+        Init();
+    }
+
+    public virtual void Init() 
+    { 
+        // optional to override
+    }
+
+}
+
+
+public class Playaaa : MonoSingletion<Playaaa>
+{
+    public override void Init()
+    {
+        base.Init();
+
+        Debug.Log("Player Initialized!");
+    }
+
+}
