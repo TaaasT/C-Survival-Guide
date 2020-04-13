09a7d6c7989855dc099b0cb350e3264df512dfb1
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-13 11:10:44 +0200

    singl. Cha

---
+++ C# Survival Guide/Assets/Scripts/Singletons/SingletionCha.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletionCha : MonoBehaviour
+{
+    private static SingletionCha _instance;
+    public static SingletionCha Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("Is null");
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
