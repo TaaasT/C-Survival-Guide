1958e626ca7f568751abeb3e2cef0920c80c80e2
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-13 11:05:51 +0200

    00

---
+++ C# Survival Guide/Assets/Scripts/Singletons/SingUIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingUIManager : MonoBehaviour
+{
+    private static SingUIManager _instance;
+    public static SingUIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("UIManager is null");
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+}
