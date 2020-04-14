48f0188a72067dcd1fa87f8fa8270ac572116b26
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-14 10:57:01 +0200

    Object Pooling

---
+++ C# Survival Guide/Assets/Scripts/Object Pooling/OBJPooling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBJPooling : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab);
+        }
+    }
+}
