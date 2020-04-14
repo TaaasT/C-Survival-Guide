571b1aaec6cafb64639cd8f5e3e5484580bf507a
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-14 11:28:04 +0200

    challenge

--- C# Survival Guide/Assets/Scripts/Object Pooling/OBJbullet.cs
+++ C# Survival Guide/Assets/Scripts/Object Pooling/OBJbullet.cs
@@ -7,7 +7,7 @@ public class OBJbullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 1f);
+        //Destroy(this.gameObject, 1f);
     }
 
     // Update is called once per frame
---
+++ C# Survival Guide/Assets/Scripts/Object Pooling/PoolManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PoolManager : MonoBehaviour
+{
+    private static PoolManager _instance;
+    public static PoolManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.Log("Is null");
+
+            return _instance;
+        }
+    }
+    [SerializeField]
+    private GameObject _bulletContainer;
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    [SerializeField]
+    private List<GameObject> _bulletPool;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        _bulletPool = GenerateBullets(10);
+    }
+
+    List<GameObject> GenerateBullets(int amountOfBullets)
+    {
+        for (int i = 0; i < amountOfBullets; i++)
+        {
+            GameObject bullet = Instantiate(_bulletPrefab);
+            bullet.transform.parent = _bulletContainer.transform;
+            bullet.SetActive(false);
+            _bulletPool.Add(bullet);
+        }
+
+        return _bulletPool;
+    }
+}
