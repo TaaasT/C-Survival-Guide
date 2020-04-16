ac2660c2f50d439ae2612ab54aaeaa9af1453f5a
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-16 09:54:46 +0200

    CommandManager pt1

---
+++ C# Survival Guide/Assets/Scripts/Command Pattern/Managers/CommandManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandManager : MonoBehaviour
+{
+    private static CommandManager _instance;
+    public static CommandManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.Log("The CommandManager is NULL");
+
+            return _instance;
+        }
+    }
+
+    private List<ICommand> _commandBuffer = new List<ICommand>();
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
+
+}
