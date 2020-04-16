c2bec48b2d10e0a7a140e6feff8662922ba45503
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-04-16 10:35:04 +0200

    Comm. Pattern Implementation

---
+++ C# Survival Guide/Assets/Scripts/Command Pattern/CPPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPPlayer : MonoBehaviour
+{
+    IICommand moveUp, moveDown, moveLeft, moveRight;
+    [SerializeField]
+    private float _speed = 2f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveUp = new MoveUpCommand(this.transform, _speed);
+            moveUp.Execute();
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            moveDown = new MoveDownCommand(this.transform, _speed);
+            moveDown.Execute();
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            moveLeft = new MoveLeftCommand(this.transform, _speed);
+            moveLeft.Execute();
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            moveRight = new MoveRightCommand(this.transform, _speed);
+            moveRight.Execute();
+        }
+    }
+}
---
+++ C# Survival Guide/Assets/Scripts/Command Pattern/MoveRightCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRightCommand : IICommand
+{
+    private Transform _player;
+    private float _speed;
+
+    public MoveRightCommand(Transform player, float speed)
+    {
+        this._player = player;
+        this._speed = speed;
+    }
+
+    public void Execute()
+    {
+        _player.Translate(Vector3.right * _speed * Time.deltaTime);
+    }
+
+    public void Undo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
